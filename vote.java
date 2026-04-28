a83b12cc7e9a3214033db749f40294d17e47dada
ankan <ankanmajumdar812@gmail.com>
2026-04-28 16:16:49 +0530

    vote management system

--- vote.java
+++ vote.java
@@ -1,48 +1,64 @@
 import java.util.Scanner;
+
 public class vote {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    int a = 0, b = 0, c = 0, d = 0, e = 0, v = 0;
-    System.out.println("Total 5 party. AREA: NETAJI COLONY.");
-    System.out.println("TOTAL PEOPLE: 10. ALL PARTY WITH CODE:");
-    System.out.println("11 FOR ~AM");
-    System.out.println("21 FOR +RT");
-    System.out.println("31 FOR #LK");
-    System.out.println("41 FOR $EP");
-    System.out.println("51 FOR @FW");
-    // now vote system.
-    System.out.println("GIVE VOTES CAREFULLY-->");
-    for (int i = 1; i <= 10; i++) {
-      System.out.print("ENTER DIGIT SIR/MA'AM: ");
-      int x = in.nextInt();
-      switch (x) {
-      case 11:
-        a++;
-        break;
-      case 21:
-        b++;
-        break;
-      case 31:
-        c++;
-        break;
-      case 41:
-        d++;
-        break;
-      case 51:
-        e++;
-        break;
-      default:
-        System.out.print("ERROR");
-        v = 1;
+
+    int totalVoters = 10;
+
+    // Party codes and names
+    int[] codes = {11, 21, 31, 41, 51};
+    String[] parties = {"~AM", "+RT", "#LK", "$EP", "@FW"};
+    int[] votes = new int[5];
+
+    System.out.println("AREA: NETAJI COLONY");
+    System.out.println("Total Voters: " + totalVoters);
+
+    System.out.println("Party Codes:");
+    for (int i = 0; i < codes.length; i++) {
+      System.out.println(codes[i] + " for " + parties[i]);
+    }
+
+    System.out.println("\n--- Start Voting ---");
+
+    for (int i = 1; i <= totalVoters; i++) {
+      System.out.print("Voter " + i + ", enter your vote: ");
+      int input = in.nextInt();
+
+      boolean valid = false;
+
+      for (int j = 0; j < codes.length; j++) {
+        if (input == codes[j]) {
+          votes[j]++;
+          valid = true;
+          break;
+        }
       }
-      if (v == 1) {
-        break;
+
+      if (!valid) {
+        System.out.println("Invalid vote! Try again.");
+        i--; // repeat same voter
       }
     }
-    System.out.println("TOTAL VOTE OF ~AM:" + a);
-    System.out.println("TOTAL VOTE OF +RT:" + b);
-    System.out.println("TOTAL VOTE OF #LK:" + c);
-    System.out.println("TOTAL VOTE OF $EP:" + d);
-    System.out.println("TOTAL VOTE OF @FW:" + e);
+
+    // Display results
+    System.out.println("\n--- Results ---");
+    int maxVotes = -1;
+    int winnerIndex = -1;
+
+    for (int i = 0; i < votes.length; i++) {
+      System.out.println(parties[i] + ": " + votes[i]);
+
+      if (votes[i] > maxVotes) {
+        maxVotes = votes[i];
+        winnerIndex = i;
+      }
+    }
+
+    // Winner
+    System.out.println("\nWinner: " + parties[winnerIndex] + " with " +
+                       maxVotes + " votes");
+
+    in.close();
   }
 }
